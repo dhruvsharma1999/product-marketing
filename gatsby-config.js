6e246bf0d8869e1f1bbabf9971c336e19b04c65c
dhruvsharma1999 <dhruvshrm34@gmail.com>
2021-08-03 22:05:07 +0530

    added gatsby template and tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Koffee Startup ",
   },
-  plugins: [],
+  plugins: ["gatsby-plugin-postcss"],
 };
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container mx-auto">
+      <p class="font-sans ...">About Page</p>
+    </div>
+  );
+};
+
+export default About;
